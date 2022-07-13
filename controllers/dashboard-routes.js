1fbea8f6ade59092c4e613a245067444276c832e
Gilina McBride <gilinamcbride@gmail.com>
2022-07-13 18:53:21 -0400

    dashboard pages and create/delete/update blogs

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -33,4 +33,42 @@ router.get("/", withAuth, (req, res) => {
     });
 });
 
+router.get("/create", withAuth, (req, res) => {
+  res.render("create-post");
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Blog.findByPk(req.params.id, {
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (dbBlogData) {
+        const blog = dbBlogData.get({ plain: true });
+
+        res.render("edit-blog", {
+          blog,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
